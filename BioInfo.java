1c8e8b9b13c2bcb0fb5a03ca47cd0f1cc0beabeb
susmita66 <susmitakarmaker69@gmail.com>
2024-08-11 00:34:42 +0600

    code-1

---
+++ BioInfo.java
@@ -0,0 +1,14 @@
+
+package javacode;
+
+
+public class BioInfo {
+    
+    public static void main(String[] args){
+        
+        System.out.println(" \"Susmita Karmaker , Hema Karmaker\" ");
+        
+        
+    }
+    
+}
