33d6a76e4b031d66ff09e69952cd72e9e9b3fe9d
susmita66 <susmitakarmaker69@gmail.com>
2024-08-11 00:44:05 +0600

    code-2

---
+++ Product2.java
@@ -0,0 +1,23 @@
+
+package assignment2;
+
+public class Product2 {
+    
+    public static void main(String [] args){
+        
+        int id = 101;
+        String title = "iphone14";
+        String price = "1850 euros";
+        String description = "perfect product with best image quality";
+        String category = "phone";
+        
+        
+        System.out.println("ID = "+id);
+        System.out.println("Title = "+title);
+        System.out.println("Price = "+price);
+        System.out.println("Description = "+description);
+        System.out.println("Category = "+category);
+        
+    }
+    
+}
