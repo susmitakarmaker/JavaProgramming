1b5414db5937399f16c18815eca89f1eedaafb3f
susmita66 <susmitakarmaker69@gmail.com>
2024-08-22 23:35:31 +0600

    code-6

---
+++ ConditionalOperator.java
@@ -0,0 +1,23 @@
+
+package begineerjava;
+
+import java.util.Scanner;
+
+public class ConditionalOperator {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int num1,num2,large;
+        System.out.print("Enter two numbers : ");
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+        
+        large = (num1>num2)?num1:num2;
+        System.out.println("Large number = "+large);
+        
+        
+    }
+    
+}
